7066222bb7d19e8c700d262dfe33e725e6efeb9a
Casper S <42980888+BitesizedLion@users.noreply.github.com>
2019-08-20 21:25:53 +0200

    Update trend.js

--- models/trend.js
+++ models/trend.js
@@ -1,9 +1,5 @@
 /*jshint esversion: 8 */
-//Name of folder shld be changed to models
 const mongoose = require("mongoose");
-
-/*  schema for how/what shld be in the document stored in the DB... add min/max lenght and req and uniq and check type
-    later. works as a second layer of protection, ensure that DB stores valid data. */
 const keywordSchema = new mongoose.Schema({
   keyword: {
     type: String,
@@ -23,7 +19,6 @@ const keywordSchema = new mongoose.Schema({
   }
 });
 
-//creates a mongoose model for creating users
 const Keyword = mongoose.model("Keyword", keywordSchema);
 
 module.exports.Keyword = Keyword;
